bcb9f491fc0e471e6158b7970b88be62035c867f
liaoxuezhi <liaoxuezhi@icloud.com>
2017-04-28 13:36:14 +0800

    Revert "Update index.js"

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var path = require('path');
 var prompt = require('prompt');
 var Client = require('ssh2').Client;
 var streamifier = require('streamifier');
-var mkdirp = require('mkdirp');
+
 
 
 function deploy(options, release, content, file, callback) {
@@ -23,8 +23,7 @@ function deploy(options, release, content, file, callback) {
       }
 
       var filepath = path.join(options.to, release);
-      filepath = filepath.replace(/\\/g,'/');
-      mkdirp(path.dirname(filepath), function(err) {
+      sftp.mkdir(path.dirname(filepath), function(err) {
         // if (err) {
         //   console.log(err, 'Mkdir failed: %s', path.dirname(filepath));
         //   return callback(err);
